b1cab636aa02dc2ab4c103037c938fc7270ae8b5
macefti276 <macefti276@gmail.com>
2018-07-09 15:15:47 +0600

    database with value

--- retrepair.sql
+++ retrepair.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2018 at 10:56 AM
+-- Generation Time: Jul 09, 2018 at 11:15 AM
 -- Server version: 10.1.33-MariaDB
 -- PHP Version: 7.2.6
 
@@ -35,20 +35,12 @@ CREATE TABLE `alerts` (
   `AlertDescription` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `persons`
+-- Dumping data for table `alerts`
 --
 
-CREATE TABLE `persons` (
-  `MessageID` int(11) NOT NULL,
-  `RecieverID` int(11) DEFAULT NULL,
-  `SenderID` int(11) DEFAULT NULL,
-  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `MessageDescription` varchar(255) DEFAULT NULL,
-  `SeenStatus` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `alerts` (`AlertID`, `UserID`, `Time`, `AlertDescription`) VALUES
+(1, 2, '2018-07-09 09:14:59', 'New Request');
 
 -- --------------------------------------------------------
 
@@ -68,6 +60,13 @@ CREATE TABLE `requests` (
   `Status` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `requests`
+--
+
+INSERT INTO `requests` (`RequestID`, `CustomerID`, `ExpertID`, `ServiceType`, `ProblemDescription`, `PaymentDescription`, `RequestingDate`, `Payment`, `Status`) VALUES
+(1, 3, 2, 'Computer', 'Keyboard problem', NULL, '2018-07-09 09:11:21', NULL, 'Pending');
+
 -- --------------------------------------------------------
 
 --
@@ -81,6 +80,13 @@ CREATE TABLE `reviews` (
   `ReviewDescription` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`ReviewID`, `CustomerID`, `ExpertID`, `ReviewDescription`) VALUES
+(1, 3, 2, 'Good Service');
+
 -- --------------------------------------------------------
 
 --
@@ -92,6 +98,13 @@ CREATE TABLE `skill` (
   `ExpertID` int(11) DEFAULT NULL,
   `SkillTitle` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `skill`
+--
+
+INSERT INTO `skill` (`SkillID`, `ExpertID`, `SkillTitle`) VALUES
+(1, 2, 'Computer');
 
 -- --------------------------------------------------------
 
@@ -113,6 +126,15 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `Name`, `Email`, `Password`, `Address`, `Type`, `PhoneNumber`, `Rating`, `MinimumPayment`, `SignInDate`) VALUES
+(1, 'Rajesh', 'rajesh@gmail.com', '123', 'Narayangonj', 'Admin', '019xxxxxxxx', NULL, NULL, '2018-07-09 09:05:50'),
+(2, 'Efti', 'efti@gmail.com', '123', 'Mirpur', 'Expert', '019xxxxxxxx', 5, 100, '2018-07-09 09:06:44'),
+(3, 'Toma', 'toma@gmail.com', '123', 'Mirpur', 'User', '019xxxxxxxx', NULL, NULL, '2018-07-09 09:07:31');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -121,12 +143,6 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `alerts`
   ADD PRIMARY KEY (`AlertID`);
-
---
--- Indexes for table `persons`
---
-ALTER TABLE `persons`
-  ADD PRIMARY KEY (`MessageID`);
 
 --
 -- Indexes for table `requests`
@@ -160,37 +176,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `alerts`
 --
 ALTER TABLE `alerts`
-  MODIFY `AlertID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `persons`
---
-ALTER TABLE `persons`
-  MODIFY `MessageID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `AlertID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `skill`
 --
 ALTER TABLE `skill`
-  MODIFY `SkillID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `SkillID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
